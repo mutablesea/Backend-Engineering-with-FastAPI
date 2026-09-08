b3bf234bc2c90e29ed36f251115812dfc86d261e
mutablesea <themutablesea113@gmail.com>
2026-09-08 17:24:14 +0600

    practice codes

--- Class-01/Practice-Codes/variable.py
+++ Class-01/Practice-Codes/variable.py
@@ -1,6 +1,7 @@
 name = "Tyler"
 age = 32
 print(name, age) # It will print Tyler 32
+# nUmbErS aNd sTriNgS aRe imMutAbLe
 score = 10
 score = score + 11
 print(score) # It will print 21
---
+++ Class-02/Practice-Codes/list.py
@@ -0,0 +1,59 @@
+numbers_list = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+numbers = [11, 22, 33, 44, 55]
+a = [1, 2, 3, 4, 5]
+print(numbers_list)
+print(type(numbers_list))
+
+print(f"The 'First' number on the list is {numbers_list[0]}")
+print(f"The 'Second' number on the list is {numbers_list[1]}")
+print(f"The 'Last' number on the list is {numbers_list[-1]}")
+print(f"The 'Second Last' number on the list is {numbers_list[-2]}")
+
+# len() tO fInD tHe lEngTh
+print(f"The Length of the list is {len(numbers_list)}")
+
+# list iS muTaBlE
+numbers_list[6] = 75
+numbers_list[-1] = 99
+print(numbers_list)
+
+print("==============================")
+# mIx tYpE & nEstEd list
+data = [10, "Python", 3.1416, False]
+print(data)
+print(data[2])
+
+students = [
+    ["Tyler Durden", 89, 6.1],
+    ["John Wick", 85],
+    ["Toji Fushiguro", 91, 6.3]
+]
+print(students)
+print(students[2])
+print(students[0][2])
+
+print("==============================")
+# list sLiCiNg
+
+
+print("==============================")
+# list iS muTaBlE
+print(numbers)
+num = numbers
+modify_index = int(input("Enter the index position to change: "))
+modify_value = int(input("Enter the new value: "))
+num[modify_index] = modify_value
+print(f"This is 'number' variable list {numbers}")
+print(f"This is 'num' variable list {num}")
+print("In both 'num' & 'number' list value has modified")
+
+print("==============================")
+# iNdePeNdEnT cOpY
+print(a)
+b = a.copy()
+modify_index = int(input("Enter the index position to change: "))
+modify_value = int(input("Enter the new value: "))
+b[modify_index] = modify_value
+print(f"This is 'a' variable list {a}")
+print(f"This is 'b' variable list {b}")
+print("Only in 'b' list value has modified")
