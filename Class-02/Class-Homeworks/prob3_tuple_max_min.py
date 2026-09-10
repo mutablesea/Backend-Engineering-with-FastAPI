d25da1498055bd42a86af94beee9b4a8bc975fb2
mutablesea <themutablesea113@gmail.com>
2026-09-10 23:55:52 +0600

    homework update problem 3,4

---
+++ Class-02/Class-Homeworks/prob3_tuple_max_min.py
@@ -0,0 +1,7 @@
+numbers_tuple = (10, 33, 90, 30, 77, 85, 57)
+print(numbers_tuple)
+
+maximum_value = max(numbers_tuple)
+minimum_value = min(numbers_tuple)
+
+print(f"Maximum value is {maximum_value} \nMinimum value is {minimum_value}")
---
+++ Class-02/Class-Homeworks/prob4_rm_dup_ele.py
@@ -0,0 +1,5 @@
+numbers_list = [1, 2, 2, 3, 3, 4, 5, 5]
+print(f"Before: {numbers_list}")
+unique_list = list(set(numbers_list))
+
+print(f"Remove duplicates form array 'numbers_list': {unique_list}")
