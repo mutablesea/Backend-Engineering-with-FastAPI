f5d8bbc976452cf74dc375713ad5eff24884eaab
mutablesea <themutablesea113@gmail.com>
2026-09-11 01:55:48 +0600

    class homework update problem 5

---
+++ Class-01/Class-Homeworks/prob5_mem_with_id.py
@@ -0,0 +1,10 @@
+a = 200
+b = 200
+print(a is b)
+
+c = 2000
+d = 2000
+print(c is d)
+
+# Explanation: In Terminal, they work differently one for True one for False but in IDE, they work
+# same both is True here.
