1b1928a6dc1600219da7c8c8043fde9831e638bd
mutablesea <themutablesea113@gmail.com>
2026-09-11 02:01:37 +0600

    class homework problem 5 update

--- Class-01/Class-Homeworks/prob5_mem_with_id.py
+++ Class-01/Class-Homeworks/prob5_mem_with_id.py
@@ -6,5 +6,9 @@ c = 2000
 d = 2000
 print(c is d)
 
-# Explanation: In Terminal, they work differently one for True one for False but in IDE, they work
-# same both is True here.
+# Explanation: In 'a' and 'b' values are from -5 to 256 ,and it saves to cache. So they are
+# pointing to the same object. But in 'c' and 'd' values are above 256. So they are pointing
+# to the different object. So, in the first situation it prints 'True' but in second situation
+# it prints 'False'
+# NB: In Terminal, they work differently one for True one for 'False' but in IDE, they work
+# same both is 'True' here.
